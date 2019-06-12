c8193b2522accabc0c87e6f1aba942a347034eeb
sagarprabhu <sagarprabhu3@gmail.com>
2019-06-11 23:48:29 -0400

    added changes for deployment to heroku

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const catalogRouter = require('./routes/catalog');
 
 var app = express();
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://sagar:yomanyo@cluster0-7su9p.mongodb.net/test?retryWrites=true&w=majority'
+var dev_db_url = 'mongodb+srv://sagar:coolpass@cluster0-7su9p.mongodb.net/test?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB,{useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console,'MongoDB connection error:'));
